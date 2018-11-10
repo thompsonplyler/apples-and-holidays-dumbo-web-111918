cef2f951708aadb46c3849d7ee59466d9ce9ce1b
Thompson Plyler <nealok.g@gmail.com>
2018-11-10 16:35:34 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -93,6 +93,7 @@ gear_array.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
+  
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
   #   Christmas: Lights, Wreath
