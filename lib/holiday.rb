d339bb1aee8fe391991e76634b72a5d5b6ed00b5
Thompson Plyler <nealok.g@gmail.com>
2018-11-10 15:59:31 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -52,7 +52,10 @@ def add_supply_to_memorial_day(holiday_hash, supply)
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-holiday_hash[season][holiday_name][supply_array]
+
+  holiday_hash.each |season_key,holiday|
+end
+
 end
 
 # given that holiday_hash looks like this:
