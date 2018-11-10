af66edb91c9f820b163721070b2098afecf6f7a5
Thompson Plyler <nealok.g@gmail.com>
2018-11-10 16:10:52 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -55,7 +55,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 
   holiday_hash.each do |season_key,holiday_value|
     if season_key == season
-      holiday_hash[season] << holiday_name
+      holiday_hash[season] = holiday_name
     end
   end
 binding.pry
